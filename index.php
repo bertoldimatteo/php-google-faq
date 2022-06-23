54ec6fe6bd9378affc9e96a10aa9277103721a08
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-06-23 14:21:40 +0200

    completato ciclo informazioni array php

--- index.php
+++ index.php
@@ -9,6 +9,50 @@
             <p>We look forward to working closely with data protection authorities and others over the coming months as we refine our approach. The CJEU's ruling constitutes a significant change for search engines. While we are concerned about its impact, we also believe that it's important to respect the Court's judgement and we are working hard to devise a process that complies with the law.</p>
             <p>When you search for a name, you may see a notice that says that results may have been modified in accordance with data protection law in Europe. We’re showing this notice in Europe when a user searches for most names, not just pages that have been affected by a removal.</p>
             ",
+        ],
+        [
+            'title' => '<h2>How does Google protect my privacy and keep my information secure?</h2>',
+            'answer' => "
+            <p>We know that security and privacy are important to you - and they are important to us, too. We make it a priority to provide strong security and give you confidence that your information is safe and accessible when you need it.</p>
+            <p>We’re constantly working to ensure strong security, protect your privacy and make Google even more effective and efficient for you. We spend hundreds of millions of dollars every year on security, and employ world-renowned experts in data security to keep your information safe. We've also built easy-to-use privacy and security tools like Google Dashboard, 2-step verification and Ads Settings. So when it comes to the information you share with Google, you’re in control.</p>
+            <p>You can find out more about safety and security online, including how to protect yourself and your family online, at the Google Safety Centre.</p>
+            <p>Find out more about how we keep your personal information private and safe – and put you in control.</p>
+            ",
+        ],
+        [
+            'title' => '<h2>Why is my account associated with a country?</h2>',
+            'answer' => "
+            <ol type='1'>
+            <p>Your account is associated with a country (or territory) in the Terms of Service so that we can determine two things:</p>
+            <li>The Google affiliate that provides the services, that processes your information, and that is responsible for complying with applicable privacy laws. Generally, Google offers its consumer services through either of two companies:</li>
+            <ol type='a'>
+            <li>Google Ireland Limited, if you’re located in the European Economic Area (EU countries plus Iceland, Liechtenstein, and Norway) or Switzerland</li>
+            <li>Google LLC, based in the United States, for the rest of the world</li>
+            </ol>
+            <li>The version of the terms that govern our relationship, which can vary depending on local laws</li>
+            </ol>
+            <p>Keep in mind that Google services are essentially the same, regardless of the affiliate that provides the services or your country association.</p>
+            ",
+        ],
+        [
+            'title' => '<h3>Determining the country associated with your account</h3>',
+            'answer' => "
+            <p>When you create a new account, we associate your account with a country based on where you created your Google Account. For accounts at least a year old, we use the country from which you usually access Google services – typically where you’ve spent the most time in the last year.</p>
+            <p>Frequent travel doesn’t generally affect the country associated with your account. If you move to a new country, it can take about a year for your country association to update.</p>
+            <p>If the country associated with your account doesn’t correspond to your country of residence, it could be because of a difference between your country of work and residence, because you’ve installed a Virtual Private Network (VPN) to mask your IP address, or because you live close to a territorial border. Contact us if you think that your country association is wrong.</p>
+            ",
+        ],
+        [
+            'title' => '<h2>How can I remove information about myself from Google’s search results?</h2>',
+            'answer' => "
+            <p>Google search results are a reflection of the content publicly available on the web. Search engines can't remove content directly from websites, so removing search results from Google wouldn't remove the content from the web. If you want to remove something from the web, you should contact the webmaster of the site the content is posted on and ask him or her to make a change. Additionally, if under European data protection law, you would like to request removal of certain information about you that appears in Google's search results, please click here. Once the content has been removed and Google has noted the update, the information will no longer appear in Google's search results. If you have an urgent removal request, you can also visit our help page for more information.</p>
+            ",
+        ],
+        [
+            'title' => '<h2>Are my search queries sent to websites when I click on Google Search results?</h2>',
+            'answer' => "
+            <p>In some cases, yes. When you click on a search result in Google Search, your web browser may also send the Internet address, or URL, of the search results page to the destination web page as the Referrer URL. The URL of the search results page may sometimes contain the search query that you entered. If you are using SSL Search (Google’s encrypted search functionality), under most circumstances, your search terms will not be sent as part of the URL in the Referrer URL. There are some exceptions to this behaviour, such as if you are using some less popular browsers. More information on SSL Search can be found here. Search queries or information contained in the Referrer URL may be available via Google Analytics or an application programming interface (API). In addition, advertisers may receive information relating to the exact keywords that triggered an ad click.</p>
+            ",
         ]
     ]
 ?>
